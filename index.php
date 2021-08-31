9e6443081360a4db10cd230f2e3590b9bd43723a
Pepe44DEv <pepe44y@gmail.com>
2021-08-31 13:47:34 +0200

    added Design added file read logic

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+error_reporting(0);
+
+
+
 
 if(isset($_GET['close'])){
     $firstname = $_GET["firstname"].PHP_EOL;
@@ -8,6 +12,13 @@ if(isset($_GET['close'])){
 
 
 }
+
+if(isset($_GET['delete'])) {
+    unlink('list.txt');
+}
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="de">
@@ -19,8 +30,17 @@ if(isset($_GET['close'])){
 <body>
 
 
-<h1 class="title">Room Manager</h1>
-<ol id="Room1">
+<h1 class="title">Projekt Manager</h1>
+<h3 class="info">Trage deinen Namen in die liste ein wenn du mitarbeiten möchtest</h3>
+
+
+
+<form class="formenter" action="index.php" method="get">
+    <input id="inputName" name="firstname" type="text" placeholder="Name eingeben">
+    <input class="enterbutton" type="submit" name="close" value="Eintragen">
+</form>
+
+<ol class="Room1">
     <?php $handle = fopen('list.txt', 'r');
 
     if ($handle) {
@@ -34,11 +54,10 @@ if(isset($_GET['close'])){
 
     ?>
 </ol>
+</ol>
 
-
-<form action="index.php" method="get">
-    <input type="submit" name="close" value="close">
-    <input id="inputName" name="firstname" type="text" placeholder="Name eingeben">
+<form class="formdelete" action="index.php" method="get">
+    <input class="delete" type="submit" name="delete" value="delete">
 </form>
 
 
